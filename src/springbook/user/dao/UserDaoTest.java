c15d818d7f768ff4d073d6642ce7b256dc6a94b0
kjnam <kyujnam@gmail.com>
2016-04-21 21:41:15 +0900

    [ADD] 학습테스트 코드 추가

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,11 +1,19 @@
 package springbook.user.dao;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -14,20 +22,40 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by kjnam on 2016. 4. 18..
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
+
+    @Autowired
+//    private ApplicationContext context;
+
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+
+    @Before
+    public void setUp() {
+
+//        this.dao = context.getBean("userDao", UserDao.class);
+
+        dao = new UserDao();
+        DataSource dataSource = new SingleConnectionDataSource(
+                "jdbc:mysql://192.168.56.101:3306/testdb", "testuser", "java41", true);
+        dao.setDataSource(dataSource);
+
+
+        this.user1 = new User("Gildong","Nam","1234");
+        this.user2 = new User("Chulsu","Kim","4567");
+        this.user3 = new User("Gibum","Park","8912");
+
+    }
 
     @Test
     public void addAndGet() throws SQLException, ClassNotFoundException {
-
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user1 = new User("Gildong","Nam","1234");
-        User user2 = new User("Chulsu","Kim","4567");
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
-
 
         dao.add(user1);
         dao.add(user2);
@@ -45,14 +73,6 @@ public class UserDaoTest {
 
     @Test
     public void count() throws SQLException, ClassNotFoundException {
-
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user1 = new User("Gildong","Nam","1234");
-        User user2 = new User("Chulsu","Kim","4567");
-        User user3 = new User("Gibum","Park","8912");
-
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
@@ -69,10 +89,6 @@ public class UserDaoTest {
 
     @Test(expected = EmptyResultDataAccessException.class)
     public void getUserFailure() throws SQLException, ClassNotFoundException {
-
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
